2c0662c3c0b0660cb52eff140c194c8fd1fe1c3f
ByteZO <coder.gny@gmail.com>
2024-09-28 21:32:21 +0530

    Refactor Home component layout and navigation

--- User Potal/src/Components/Home.jsx
+++ User Potal/src/Components/Home.jsx
@@ -4,17 +4,23 @@ import { Link, Outlet } from 'react-router-dom';
 function Home() {
   return (
     <>
-      <nav className="bg-gray-800 p-4">
+      <nav className="bg-gray-800 p-4 shadow-lg">
         <div className="container mx-auto flex justify-between items-center">
           <h1 className="text-2xl text-white font-bold">PharmaScout</h1>
           <div>
-            <Link to='/signup' className="text-white hover:text-gray-300 px-4">Sign Up</Link>
-            <Link to='/login' className="text-white hover:text-gray-300 px-4">Login</Link>
+            <Link 
+              to='/login' 
+              className="text-white border-2 border-white hover:bg-gray-950 hover:text-white px-6 py-3 rounded-lg transition duration-300 font-semibold"
+            >
+              Login
+            </Link>
           </div>
         </div>
       </nav>
       <div className="flex items-center justify-center h-screen bg-gradient-to-r from-green-400 to-blue-500">
-        <h1 className="text-4xl font-bold text-white drop-shadow-lg">Welcome to PharmaScout!</h1>
+        <h1 className="text-5xl font-extrabold text-white drop-shadow-lg">
+          Welcome to PharmaScout!
+        </h1>
       </div>
       <Outlet />
     </>
